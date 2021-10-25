0b3c17e56677b3ea66a84a11e43ab351f512223f
qi <q13264900530@163.com>
2021-10-25 17:06:22 +0800

    init

--- src/view/Home/index.tsx
+++ src/view/Home/index.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import styled from "styled-components";
-<<<<<<< HEAD
-import { LeftMenu, About, Avatar, Editor, ModalWrapper } from 'components';
-import { Flex } from 'uikit';
-
-import { mediaQueriesSize } from "uikit/theme/base";
-
-=======
 import { About, Avatar, Editor, ModalWrapper } from 'components';
 import {Route} from 'react-router-dom'
 import { Flex ,Box} from 'uikit';
@@ -14,7 +7,6 @@ import {Menu}  from './left';
 import {Header,Tabs,ArticleList}  from './center';
 import {Search,Swap,RecommendPeople,HotTopic,FooterCopyright}  from './right';
 import { mediaQueries, mediaQueriesSize } from "uikit/theme/base";
->>>>>>> origin/main
 const PageContainer = styled.div`
   width: 1200px;
   margin: 0 auto;
@@ -25,13 +17,8 @@ justify-content:center;
 const LeftCard = styled(Flex)`
   // width: 375px;
 `
-<<<<<<< HEAD
-export const CenterCard = styled(Flex)`
-  flex: 1;
-=======
 const CenterCard = styled(Box)`
   // flex: 1;
->>>>>>> origin/main
   ${mediaQueriesSize.marginLRmd}
   // width:670px;
 `
@@ -90,15 +77,12 @@ const Home: React.FC = () => {
           <Route path="/" component={ArticleList}></Route>
         </CenterCard>
         <RightCard>
-<<<<<<< HEAD
-          
-=======
           <Route path="/" component={Search}></Route>
           <Route path="/" component={Swap}></Route>
           <Route path="/" component={RecommendPeople}></Route>
           <Route path="/" component={HotTopic}></Route>
           <Route path="/" component={FooterCopyright}></Route>
->>>>>>> origin/main
+
         </RightCard>
       </Flex>
       {/* <button>Open Modal</button>
