500466ae51dfb038c42b6988da79d7d13b69a852
wanghao <1750110604@qq.com>
2021-10-26 09:33:19 +0800

    通知

--- src/view/News/Notice/index.tsx
+++ src/view/News/Notice/index.tsx
@@ -14,7 +14,11 @@ const NewsNotice: React.FC = () => {
     <NoticeWrapper>
       <Header title={'消息'} />
       <div className="notice-content-wrapper">
-        
+        {
+          list.map((item: any, index: number) => {
+            return <NoticeItem key={index} />
+          })
+        }
       </div>
     </NoticeWrapper>
   )
@@ -23,7 +27,16 @@ const NewsNotice: React.FC = () => {
 const NoticeItem: React.FC = () => {
   return (
     <NoticeItemWrapper>
-
+       <div className={`notice-wrapper`}>
+       <div className="avatar"></div>
+        <div className="notice-info">
+          <div className="notice-name">
+            <h3>Dinosaur Sofi</h3>
+            <span>12:55</span>
+          </div>
+          <div className="notice-des"><span>平台通知：</span>2021年10月14日，新增opensea NFT</div>
+        </div>
+      </div>
     </NoticeItemWrapper>
   )
 }
