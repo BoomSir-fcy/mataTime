503b3c0b5e2792bf3f7009e4abe8f9bc82fc02eb
kksam <1011847054@qq.com>
2021-11-13 15:27:57 +0800

    fix: update common left menu

--- src/view/Home/left/menu.tsx
+++ src/view/Home/left/menu.tsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Flex, Box, Card } from 'uikit';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { ProfileMenu, Icon, Logo } from 'components';
+import { ProfileMenu, Icon, Logo, Badge } from 'components';
 
 import menuData from './menuData';
 
 const MenuBox = styled(Card)`
+  position: relative;
+  display: flex;
   flex-direction: column;
-  justify-content: space-between;
   padding: 20px 13px;
   width: 200px;
   height: calc(100vh - 150px);
-  min-height: 780px;
   border-radius: 10px;
 `;
 
@@ -30,36 +30,17 @@ const ItemLink = styled(Link)`
   border-radius: 18px;
   font-size: 18px;
   /* font-weight: bold; */
-  color: ${({ theme }) => theme.isDark ? '#eaeaea' : '#000000'};
+  color: ${({ theme }) => (theme.isDark ? '#eaeaea' : '#000000')};
   line-height: 40px;
 `;
-const Badge = (props: { count: number | string }) => {
-  let { count } = props;
-  if (count > 99) {
-    count = '+99';
-  }
-  return (
-    <span
-      style={{
-        position: 'absolute',
-        top: '-5px',
-        right: '-20px',
-        // width:'25px',
-        padding: '0 6px',
-        height: '15px',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        background: '#EC612B',
-        borderRadius: '5px',
-        fontWeight: 400,
-        fontSize: '12px'
-      }}
-    >
-      {count}
-    </span>
-  );
-};
+
+const User = styled(Flex)`
+  display: block;
+  margin-top: auto;
+  flex: 1;
+  flex-grow: inherit;
+`;
+
 export const MenuList = (props: { menuList: any[] }) => {
   const isDark = useSelector((state: any) => state.appReducer.systemCustom.isDark);
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -71,18 +52,18 @@ export const MenuList = (props: { menuList: any[] }) => {
     <>
       {menuList.map((item, index) => {
         return (
-          <ItemLink onClick={itemClick.bind(this, index)} to={item.path} key={index}
+          <ItemLink
+            onClick={itemClick.bind(this, index)}
+            to={item.path}
+            key={index}
             style={{
               backgroundColor: currentIndex === index ? (isDark ? '#232A3D' : '#EAF2FF') : '',
               fontWeight: currentIndex === index ? 'bold' : 'normal'
-            }}>
+            }}
+          >
             <div style={{ position: 'relative' }} title={item.title}>
-              {item.badge ? <Badge count={0}></Badge> : ''}
-              <Icon
-                name={currentIndex === index ? item.activeIcon : item.icon}
-                margin="10px 14px"
-                color={isDark ? '#ffffff' : '#7A83A0'}
-              ></Icon>
+              {item.badge && <Badge count={0} />}
+              <Icon name={currentIndex === index ? item.activeIcon : item.icon} margin="10px 14px" color={isDark ? '#ffffff' : '#7A83A0'}></Icon>
             </div>
             <span style={{ marginLeft: '5px' }}>{item.title}</span>
           </ItemLink>
@@ -100,12 +81,11 @@ export const Menu: React.FC = () => {
         <LogoWrapper>
           <Logo url="/" src={require(isDark ? 'assets/images/logo.svg' : 'assets/images/light_logo.svg').default} />
         </LogoWrapper>
-
         <MenuList menuList={menuData}></MenuList>
       </Box>
-      <Link to="/me">
+      <User as={Link} to="/me">
         <ProfileMenu />
-      </Link>
+      </User>
     </MenuBox>
   );
 };
