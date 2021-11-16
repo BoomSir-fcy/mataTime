ac26fad7e3ab6a250e15ddfcc88fcfc93444f1ec
wanghao <1750110604@qq.com>
2021-11-16 09:56:15 +0800

    首页菜单国际化

--- src/view/Home/left/menuData.ts
+++ src/view/Home/left/menuData.ts
@@ -3,44 +3,51 @@ const menuArr = [
     icon: 'icon-shouye',
     activeIcon: 'icon-shouye1',
     title: '首页',
-    path: '/'
+    path: '/',
+    transaltion: 'homeMenuHome'
   },
   {
     icon: 'icon-xingqiu',
     activeIcon: 'icon-xingqiu1',
     title: '星球',
-    path: '/'
+    path: '/',
+    transaltion: 'homeMenuStar'
   },
   {
     icon: 'icon-xiaoxi',
     activeIcon: 'icon-xiaoxi1',
     title: '消息',
-    path: '/news/me'
+    path: '/news/me',
+    transaltion: 'homeMenuNews'
   },
   {
     icon: 'icon-youxiang',
     activeIcon: 'icon-youxiang1',
     title: '私信',
     path: '/',
-    badge: true
+    badge: true,
+    transaltion: 'homeMenuLetter'
   },
   {
     icon: 'icon-qitawenti',
     activeIcon: 'icon-qitawenti1',
     title: '其他',
-    path: '/'
+    path: '/',
+    transaltion: 'homeMenuOther'
   },
   {
     icon: 'icon-qianbao',
     activeIcon: 'icon-qianbao1',
     title: '钱包',
-    path: '/'
+    path: '/',
+    transaltion: 'homeMenuWallet'
   },
   {
     icon: 'icon-w31shezhi',
     activeIcon: 'icon-a-31shezhi1',
     title: '设置',
-    path: '/set/safeset'
+    path: '/set/safeset',
+    transaltion: 'homeMenuSet'
   }
 ];
 export default menuArr;
