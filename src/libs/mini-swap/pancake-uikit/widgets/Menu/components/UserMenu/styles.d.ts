079b9e04a385cca8f03f4a75ab0d8148c04b357e
龙小乂 <43882141+A342826844@users.noreply.github.com>
2021-11-19 22:37:11 +0800

    feat: 移植mini-swap

--- src/libs/mini-swap/pancake-uikit/widgets/Menu/components/UserMenu/styles.d.ts
+++
@@ -1,3 +0,0 @@
-import { UserMenuItemProps } from "./types";
-export declare const UserMenuDivider: import("styled-components").StyledComponent<"hr", import("styled-components").DefaultTheme, {}, never>;
-export declare const UserMenuItem: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, UserMenuItemProps, never>;
