9fd40c89785cc3aaf197ef76c4694b90efc83d34
wanghao <1750110604@qq.com>
2021-11-25 20:44:21 +0800

    layout

--- src/components/Layout/CommonLayout/style.ts
+++ src/components/Layout/CommonLayout/style.ts
@@ -7,14 +7,17 @@ export const CommonLayoutWrapper = styled(Box)`
 `
 
 export const LayoutContentWrapper = styled(Box)`
-    width: 100%;
+    width: 1200px;
     display: flex;
     justify-content: center;
     padding-top: 35px;
+    margin: 0 auto;
 `
 
 export const LayoutLeftWrapper = styled(Box)`
     width: 200px;
+    height: 100vh;
+    overflow: auto;
 `
 export const LayoutMiddleWrapper = styled(Box)`
     width: 670px;
@@ -22,5 +25,8 @@ export const LayoutMiddleWrapper = styled(Box)`
     margin-right: 15px;
 `
 export const LayoutRightWrapper = styled(Box)`
-    // width: 375px;
+  width: 300px;
+  height: 100vh;
+  position: relative;
+  overflow: auto;
 `
