bc081843e2bace827cbcc16876e2c47303f0ff97
龙小乂 <43882141+A342826844@users.noreply.github.com>
2021-11-25 15:58:43 +0800

    feat： 添加左侧导航栏

--- src/view/Account/index.tsx
+++ src/view/Account/index.tsx
@@ -1,18 +1,46 @@
 import React, { useState } from 'react';
 import { Flex, Box } from 'uikit';
-import { PageSection } from 'components'
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+import { useTranslation } from 'contexts/Localization';
+import { PageSection, Crumbs, CommonMenu, Affix } from 'components'
+import { Menu } from 'view/Home/left';
 import TokenAccount from './components/TokenAccount';
 import Liquidity from './components/Liquidity';
 import Single from './components/Single';
 
+const LeftCard = styled(Flex)`
+  width: 200px;
+  height: 100vh;
+  overflow: auto;
+`;
+const CenterCard = styled(Box)`
+  flex: 1;
+  margin: 0 15px;
+  border-left: 1px solid ${({ theme }) => theme.colors.borderThemeColor};
+  border-right: 1px solid ${({ theme }) => theme.colors.borderThemeColor};
+`;
 
 const Account: React.FC = () => {
+  const { t } = useTranslation();
 
   return (
-    <PageSection>
-      <TokenAccount />
-      {/* <Liquidity /> */}
-      <Single />
+    <PageSection innerProps={{ paddingTop: '0 !important' }}>
+      <Flex justifyContent="space-between" width="100%">
+        <LeftCard>
+          <Affix offsetTop={100} positionObj={{}}>
+            {/* <CommonMenu hideGoBack menu={menuArr} history={history} /> */}
+            <Menu />
+          </Affix>
+        </LeftCard>
+        <CenterCard>
+          <Crumbs title={t('homeMenuSet')} />
+          <TokenAccount />
+          {/* <Liquidity /> */}
+          <Single />
+        </CenterCard>
+      </Flex>
+
     </PageSection>
   )
 }
