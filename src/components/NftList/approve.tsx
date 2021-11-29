267a0a109ca13a0f1b9e3018dc26f5b9ee21d2d4
BoomSir-fcy <351984524@qq.com>
2021-11-29 21:05:38 +0800

    fix:翻译

--- src/components/NftList/approve.tsx
+++ src/components/NftList/approve.tsx
@@ -16,8 +16,6 @@ const NowrapBtn = styled(Button)`
 `;
 
 export const NftButton: React.FC<{ item: any }> = ({ item }) => {
-  console.log(item);
-
   const { t } = useTranslation();
   const { account } = useWeb3React();
   const { onStake: onNftsStake } = useNftStakeFarms();
@@ -38,7 +36,7 @@ export const NftButton: React.FC<{ item: any }> = ({ item }) => {
           dispatch(fetchUserNftInfoAsync(account));
           dispatch(fetchThunk.fetchUserInfoAsync());
           setPendingTx(false);
-          toast.success('更换成功');
+          toast.success(t('NFT Replaced successfully'));
         }, 15000);
       }
     },
@@ -61,33 +59,29 @@ export const NftButton: React.FC<{ item: any }> = ({ item }) => {
               );
             } else {
               // 质押
-              await handleStakeOrUnstake(
-                1,
-                item.properties.token,
-                item.properties.token_id
-              );
-              dispatch(storeAction.setUserNftStake({ isStakeNft: true }));
-              dispatch(fetchUserNftInfoAsync(account));
-              toast.success('质押成功');
-              setPendingTx(false);
+              // await handleStakeOrUnstake(
+              //   1,
+              //   item.properties.token,
+              //   item.properties.token_id
+              // );
+              // dispatch(storeAction.setUserNftStake({ isStakeNft: true }));
+              // dispatch(fetchUserNftInfoAsync(account));
+              // toast.success('质押成功');
+              // setPendingTx(false);
             }
           } catch (e) {
             console.error(e);
-            toast.error('操作失败');
+            toast.error(t('NFT Operation failed'));
             setPendingTx(false);
           }
         } else {
-          toast.error('请先选择头像');
+          toast.error(t('NFT Please select an avatar first'));
         }
       }}
     >
       {pendingTx ? (
-        <Dots>{t('质押中')}</Dots>
-      ) : isStakeNft ? (
-        t('替换质押')
-      ) : (
-        t('质押')
-      )}
+        <Dots>{t('NFT Staking')}</Dots>
+      ) : t('NFT Replacement Stake')}
     </NowrapBtn>
   );
 };
