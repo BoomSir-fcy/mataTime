4164e02030b2522a7af8370fc5099aea0b971a99
kksam <1011847054@qq.com>
2021-12-04 18:07:56 +0800

    fix: update reward auther

--- src/components/RewardAuth/RewardAuthModal.tsx
+++ src/components/RewardAuth/RewardAuthModal.tsx
@@ -1,16 +1,14 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import BigNumber from 'bignumber.js';
 import styled from 'styled-components';
-import { toast } from 'react-toastify'
-import { Container } from 'components'
+import { DropDown } from 'components';
 import { Flex, Box, Text, Card, Button, InputPanel, Image } from 'uikit';
 import { useTranslation } from 'contexts/Localization';
-import { AvatarCard } from '../Avatar/AvatarCard'
-import { Avatar } from '../Avatar'
+import { AvatarCard } from '../Avatar/AvatarCard';
+import { Avatar } from '../Avatar';
 import QuestionHelper from 'components/QuestionHelper';
 
-
-const RewardAuthModalStyled = styled(Box) <{ top: number; left: number }>`
+const RewardAuthModalStyled = styled(Box)<{ top: number; left: number }>`
   position: fixed;
   z-index: 99;
   width: 418px;
@@ -21,49 +19,48 @@ const RewardAuthModalStyled = styled(Box) <{ top: number; left: number }>`
   top: ${({ top }) => top}px;
   left: ${({ left }) => left}px;
   background: ${({ theme }) => theme.colors.tertiary};
-  
-`
-
+`;
 const CoinSelectStyled = styled(Button)`
   width: 100px;
   height: 35px;
   padding: 0;
-`
+`;
+
 const JiantouStyled = styled.div<{ open: boolean }>`
   border: 6px solid transparent;
   border-top-color: #fff;
   transition: all 0.3s;
-  transform: ${({ open }) => open ? 'rotateZ(180deg) translateY(3px)' : 'rotateZ(0) translateY(3px)'};
+  transform: ${({ open }) =>
+    open ? 'rotateZ(180deg) translateY(3px)' : 'rotateZ(0) translateY(3px)'};
   transform-origin: center;
-`
+`;
 
 const InputPanelStyled = styled(InputPanel)`
-  padding: 0 8px;
+  padding: 6px 9px;
   min-width: 100px;
   height: 50px;
   margin-bottom: 12px;
   box-shadow: inset 0px 3px 2px 0px rgba(0, 0, 0, 0.35);
-`
+`;
 
 interface RewardAuthModalProps {
-  depositSymbol?: string
-  onConfirm?: (amount: string) => void
-  onDismiss?: () => void
-  userName?: string
-  address?: string
-  avatar?: string
-  offsetTop?: number
-  offsetLeft?: number
+  depositSymbol?: string;
+  onConfirm?: (amount: string) => void;
+  onDismiss?: () => void;
+  userName?: string;
+  address?: string;
+  avatar?: string;
+  offsetTop?: number;
+  offsetLeft?: number;
 }
 
 const RewardAuthModal: React.FC<RewardAuthModalProps> = ({
   avatar,
   offsetLeft,
-  offsetTop,
+  offsetTop
 }) => {
-  const { t } = useTranslation()
-
-  const [open, setOpen] = useState(false)
+  const { t } = useTranslation();
+  const [open, setOpen] = useState(false);
 
   return (
     <RewardAuthModalStyled left={offsetLeft} top={offsetTop}>
@@ -74,50 +71,79 @@ const RewardAuthModal: React.FC<RewardAuthModalProps> = ({
           address="0x07D4b8e2ffdE65147e18a7a192B0ed7d2133f47a"
         />
         <Flex alignItems="center">
-          <CoinSelectStyled onClick={() => setOpen(!open)}>
-            <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" width={20} height={20} />
-            <Text margin="4px">BTC</Text>
-            <JiantouStyled open={open} />
-          </CoinSelectStyled>
-          <QuestionHelper ml="15px" text={<>
-            <Text fontSize="14px">链上打赏并支持作者的创作，您的支持将会鼓励作者更大的创作热情</Text>
-            <Text fontSize="14px" color="textTips">*平台将收取0.3%的交易手续费</Text>
-          </>} />
+          <Box width="100px">
+            <CoinSelectStyled onClick={() => setOpen(true)}>
+              <Image
+                src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+                width={20}
+                height={20}
+              />
+              <Text margin="4px">BTC</Text>
+              <JiantouStyled open={open} />
+            </CoinSelectStyled>
+            <DropDown
+              fillWidth
+              isOpen={open}
+              scale="xs"
+              setIsOpen={() => setOpen(false)}
+            >
+              {[1, 2, 3, 4, 5, 6].map(item => (
+                <Box>{item}</Box>
+              ))}
+            </DropDown>
+          </Box>
+          <QuestionHelper
+            ml="15px"
+            text={
+              <>
+                <Text fontSize="14px">
+                  链上打赏并支持作者的创作，您的支持将会鼓励作者更大的创作热情
+                </Text>
+                <Text fontSize="14px" color="textTips">
+                  *平台将收取0.3%的交易手续费
+                </Text>
+              </>
+            }
+          />
         </Flex>
       </Flex>
-      <Flex flexWrap="wrap" mt="16px" justifyContent="space-between">
-        {
-          [1, 2, 3, 4, 5, 6].map(item => (
-            <InputPanelStyled>
-              <Flex alignItems="center">
-                <Box width="20px">
-                  <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" width={20} height={20} />
-                </Box>
-                <Box ml="4px">
-                  <Text bold fontSize="18px">10</Text>
-                  <Text fontSize="14px" >$10</Text>
-                </Box>
-              </Flex>
-            </InputPanelStyled>
-          ))
-        }
-      </Flex>
-      <Flex mt="4px">
-        <Flex>
-          {
-            [1, 2, 3].map(item => (
-              <Box width="11px">
-                <Box width="22px">
-                  <Avatar scale="mm" />
-                </Box>
+      <Flex flexWrap="wrap" mt="12px" justifyContent="space-between">
+        {[1, 2, 3, 4, 5, 6].map(item => (
+          <InputPanelStyled>
+            <Flex>
+              <Box width="20px">
+                <Image
+                  src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+                  width={20}
+                  height={20}
+                />
               </Box>
-            ))
-          }
+              <Flex ml="3px" flexDirection="column">
+                <Text bold ml="5px" style={{ lineHeight: 'normal' }}>
+                  10
+                </Text>
+                <Text fontSize="14px" style={{ lineHeight: 'normal' }}>
+                  $10
+                </Text>
+              </Flex>
+            </Flex>
+          </InputPanelStyled>
+        ))}
+      </Flex>
+      <Flex mt="4px" alignItems="center">
+        <Flex ml="1em">
+          {[1, 2, 3].map(item => (
+            <Box width="24px" style={{ marginLeft: '-1em' }}>
+              <Avatar scale="md" style={{ width: '24px', height: '24px' }} />
+            </Box>
+          ))}
         </Flex>
-        <Text ml="22px">共81人已打赏这篇帖子</Text>
+        <Text ml="11px" fontSize="14px" color="textTips">
+          共81人已打赏这篇帖子
+        </Text>
       </Flex>
     </RewardAuthModalStyled>
-  )
-}
+  );
+};
 
 export default RewardAuthModal;
--- src/components/RewardAuth/RewardAuthTag.tsx
+++ src/components/RewardAuth/RewardAuthTag.tsx
@@ -1,40 +1,39 @@
-import React, { useCallback } from 'react'
-import styled from 'styled-components'
-import { Flex, Box, Text, TextProps } from 'uikit'
-import { useTranslation } from 'contexts/Localization'
-import { Icon } from '../Icon'
-import useRewardAuth from 'contexts/RewardAuthContext/hooks/useRewardAuth'
+import React, { useCallback } from 'react';
+import styled from 'styled-components';
+import { Flex, Box, Text, TextProps } from 'uikit';
+import { useTranslation } from 'contexts/Localization';
+import { Icon } from '../Icon';
+import useRewardAuth from 'contexts/RewardAuthContext/hooks/useRewardAuth';
 
-interface RewardAuthProps {
-
-}
+interface RewardAuthProps {}
 
 const RewardAuthTagStyled = styled(Flex)`
   cursor: pointer;
-`
+`;
 
-export const RewardAuthTag: React.FC<RewardAuthProps> = ({ }) => {
-  const { t } = useTranslation()
-  const { setVisible } = useRewardAuth()
-  // console.log(1)
+export const RewardAuthTag: React.FC<RewardAuthProps> = ({}) => {
+  const { t } = useTranslation();
+  const { setVisible } = useRewardAuth();
 
-  const handleMounsEnter = useCallback((event) => {
-    console.log(event)
-    setVisible(true)
-  }, [])
-  const handleMounsLevel = useCallback((event) => {
-    setVisible(false)
-  }, [])
+  const handleMounsEnter = useCallback(event => {
+    console.log(event);
+    setVisible(true);
+  }, []);
+
+  const handleMounsLevel = useCallback(event => {
+    // setVisible(false);
+  }, []);
+
   return (
     <RewardAuthTagStyled
       alignItems="center"
       onMouseEnter={handleMounsEnter}
       onMouseLeave={handleMounsLevel}
     >
-      <Icon color="red" name="icon-dashang" />
+      <Icon color="red" margin="0 10px 0 0" name="icon-dashang" />
       36
     </RewardAuthTagStyled>
-  )
-}
+  );
+};
 
-export default RewardAuthTag
+export default RewardAuthTag;
