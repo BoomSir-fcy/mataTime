79c538c6439ca9accb98b048c96599a5882ad36b
A342826844 <342826844@qq.com>
2021-12-12 18:22:02 +0800

    feat: 链接钱包弹窗

--- src/components/ConnectWallet/WalletItem.tsx
+++ src/components/ConnectWallet/WalletItem.tsx
@@ -20,7 +20,7 @@ const WalletButton = styled(Button).attrs({ width: '100%', variant: 'text' })`
   justify-content: flex-start;
   margin-left: auto;
   margin-right: auto;
-  background-color: ${({ theme }) => theme.colors.backgroundCard};
+  background-color: ${({ theme }) => theme.colors.inputSelect};
   width: 200px;
   padding-top: 8px;
   padding-bottom: 8px;
