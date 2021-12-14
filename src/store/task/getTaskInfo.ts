20e9f292c95700db33ed8e985e5191ff81934f94
gww99 <95062392+gww99@users.noreply.github.com>
2021-12-14 14:09:39 +0800

    任务优化

--- src/store/task/getTaskInfo.ts
+++ src/store/task/getTaskInfo.ts
@@ -1,8 +1,9 @@
 import { Api } from 'apis';
 
 // 任务列表
-export const getTaskListData = async () => {
+export const getTaskListData = async (isSignIn?: boolean) => {
   try {
+    if (isSignIn) await Api.TaskApi.SignIn();
     const res = await Api.TaskApi.getTaskStatus();
     if (Api.isSuccess(res)) {
       return res.data || [];
