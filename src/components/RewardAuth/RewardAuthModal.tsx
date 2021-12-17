ab9132c7f932a8cc349847e54319881bec7ff67c
kksam <1011847054@qq.com>
2021-12-17 15:29:45 +0800

    fix:update

--- src/components/RewardAuth/RewardAuthModal.tsx
+++ src/components/RewardAuth/RewardAuthModal.tsx
@@ -27,7 +27,7 @@ import {
 
 import QuestionHelper from 'components/QuestionHelper';
 
-const RewardAuthModalStyled = styled(Box) <{ bottom: number; right: number }>`
+const RewardAuthModalStyled = styled(Box)<{ bottom: number; right: number }>`
   position: absolute;
   z-index: 999;
   width: 418px;
@@ -132,7 +132,7 @@ const RewardAuthModal: React.FC<RewardAuthModalProps> = ({
       setState(p => {
         p.tokenList = newArr;
         p.currentToken = newArr[0];
-        p.current_price = price.current_price || '0.12345';
+        p.current_price = price.current_price || '0';
         p.isOnApprove = newArr[0][4] > 0 ? false : true;
       });
     } catch (error) {
@@ -150,19 +150,20 @@ const RewardAuthModal: React.FC<RewardAuthModalProps> = ({
       setState(p => {
         p.reward_post = res;
       });
-    } catch (error) { }
+    } catch (error) {}
   };
 
   // 切换币种
   const changeCoinChecked = async (rows: string[], index) => {
     try {
       const res = await getPrice(rows[0]);
+      console.log(tokenList[index]);
       setState(p => {
         p.currentToken = rows;
-        p.current_price = res.current_price || '0.12345';
+        p.current_price = res.current_price || '0';
         p.isOnApprove = tokenList[index][4] > 0 ? false : true;
       });
-    } catch (error) { }
+    } catch (error) {}
   };
 
   // 打赏用户
