583ba4cf1f7fd3169fabd6d4c0d8b0fa32c0623e
龙小乂 <43882141+A342826844@users.noreply.github.com>
2021-12-25 19:08:35 +0800

    fix: 币种正则配置 返回键优化 表情加载优化

--- src/components/MenuNav/NavGoback.tsx
+++ src/components/MenuNav/NavGoback.tsx
@@ -10,7 +10,7 @@ const NavGoback: React.FC = () => {
   const { goBack, push } = useHistory()
 
   return (
-    <NavItemStyled onClick={() => goBack()} mt="1px" alignItems="center" padding="28px 14px">
+    <NavItemStyled onClick={() => push('/')} mt="1px" alignItems="center" padding="28px 14px">
       <IconBox><Icon color="textSubtle" name="icon-fanhui" /></IconBox>
       <Text ml="20px" fontSize="18px" bold color="white_black" >{t('newsBack')}</Text>
     </NavItemStyled>
