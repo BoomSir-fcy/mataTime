9d668f49a1982859bdb9bcd31ad3af09109e3079
龙小乂 <43882141+A342826844@users.noreply.github.com>
2022-01-20 09:59:21 +0800

    feat: a标签优化

--- src/components/Layout/HoverLink.tsx
+++ src/components/Layout/HoverLink.tsx
@@ -1,11 +1,46 @@
-import { Link } from 'react-router-dom';
-import styled from "styled-components";
+import { useState } from 'react';
+import { Link, LinkProps } from 'react-router-dom';
+import styled from 'styled-components';
+// import { LinkProps } from 'uikit';
 
-export const HoverLink = styled(Link)`
+export const HoverLinkStyled = styled(Link)`
   transition: background 0.3s;
   display: block;
-  &:hover, &:active, &:focus {
+  &:hover,
+  &:active,
+  &:focus {
     background: ${({ theme }) => theme.colors.hoverList};
     outline: none;
   }
-`
+`;
+
+export const HoverLink: React.FC<LinkProps> = ({ children, ...props }) => {
+  const [draging, setDraging] = useState(false);
+  return (
+    <HoverLinkStyled
+      onDragStart={event => {
+        console.log('onDragStart');
+        setDraging(true);
+        event.preventDefault();
+      }}
+      // onDragOver={}
+      // onDragOver={() => {
+      //   console.log('onDragOver');
+      //   setDraging(false);
+      // }}
+      {...props}
+      onClick={event => {
+        console.log(draging, 'draging');
+        if (draging) {
+          setDraging(false);
+          event.preventDefault();
+        }
+        if (props.onClick) {
+          props.onClick(event);
+        }
+      }}
+    >
+      {children}
+    </HoverLinkStyled>
+  );
+};
