79e3ea6be02d8dfcdc9a3eeac1e43199612d66ff
龙小乂 <43882141+A342826844@users.noreply.github.com>
2022-02-16 15:19:06 +0800

    feat: 发帖

--- src/view/Me/Tribe/components/TagList.tsx
+++ src/view/Me/Tribe/components/TagList.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import { Icon } from 'components';
+import { Icon, iocnType } from 'components';
 import styled from 'styled-components';
-import { Flex, Text } from 'uikit';
+import { Flex, Text, TextProps } from 'uikit';
 
 const TopicsContentFlex = styled(Flex)`
   flex-wrap: wrap;
 `;
-const Tag = styled(Flex)`
+export const Tag = styled(Flex)<{ cursor?: string; mr?: string }>`
   align-items: center;
   padding: 5px 10px;
-  margin-right: 20px;
+  margin-right: ${({ mr }) => mr || '20px'};
   margin-bottom: 10px;
   border: 1px solid ${({ theme }) => theme.colors.ThemeText};
   border-radius: 10px;
   text-align: center;
+  cursor: ${({ cursor }) => cursor || 'default'};
   .text {
-    margin-right: 10px;
     color: ${({ theme }) => theme.colors.textPrimary};
-    cursor: default;
+    cursor: inherit;
+    white-space: nowrap;
   }
   .icon-cancel {
+    /* margin-left: 10px; */
     cursor: pointer;
   }
 `;
@@ -28,18 +30,29 @@ type TagItem = {
   name: string;
 };
 
+export const CancleIcon: React.FC<Partial<iocnType>> = props => (
+  <Icon
+    className='icon-cancel'
+    name='icon-guanbi2fill'
+    size={13}
+    margin='0 0 0 10px'
+    color='#343434'
+    {...props}
+  />
+);
+
+export const TagText: React.FC<TextProps> = props => (
+  <Text className='text' {...props} />
+);
+
 const TagList: React.FC<{ list?: TagItem[]; onDelete?: (id: number) => void }> =
   React.memo(({ list, onDelete }) => {
     return (
       <TopicsContentFlex>
         {list.map(item => (
           <Tag key={item.id}>
-            <Text className='text'>{item.name}</Text>
-            <Icon
-              className='icon-cancel'
-              name='icon-guanbi2fill'
-              size={13}
-              color='#343434'
+            <TagText>{item.name}</TagText>
+            <CancleIcon
               onClick={() => {
                 onDelete(item.id);
               }}
