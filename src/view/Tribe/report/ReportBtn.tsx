ac05df02a4e1777f6af62d2211a84febdbb475a0
gww99 <95062392+gww99@users.noreply.github.com>
2022-03-24 17:42:29 +0800

    fix: 修复bug

--- src/view/Tribe/report/ReportBtn.tsx
+++ src/view/Tribe/report/ReportBtn.tsx
@@ -1,11 +1,13 @@
-import { Icon } from 'components';
 import React from 'react';
+import { Icon } from 'components';
+import { useTranslation } from 'contexts';
 import { NftStatus, TribeBelongNft, TribeInfo } from 'store/tribe/type';
 import { ReportTribeModal } from '.';
 
 export const ReportBtn: React.FC<{ tribeInfo: TribeInfo }> = ({
   tribeInfo,
 }) => {
+  const { t } = useTranslation();
   const [visibleReport, setVisibleReport] = React.useState(false);
   return (
     <>
@@ -14,9 +16,9 @@ export const ReportBtn: React.FC<{ tribeInfo: TribeInfo }> = ({
           tribeInfo?.status === NftStatus.UnStake ||
           tribeInfo?.status === NftStatus.Staked) && (
           <Icon
-            margin='0px 8px'
             name='icon-tousu'
             color='textgrey'
+            title={t('Report')}
             current
             onClick={() => {
               setVisibleReport(true);
